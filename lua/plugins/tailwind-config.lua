690ea29669b2f0898183f9d77a3901c40a81b453
Onizuka893 <volkanibis123@gmail.com>
2025-03-27 12:03:44 +0100

    initial

---
+++ lua/plugins/tailwind-config.lua
@@ -0,0 +1,37 @@
+vim.filetype.add({
+  extension = {
+    heex = "heex", -- Map .heex files to the 'heex' filetype
+  },
+})
+
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    setup = {
+      tailwindcss = function(_, opts)
+        local tw = LazyVim.lsp.get_raw_config("tailwindcss")
+        opts.filetypes = opts.filetypes or {}
+
+        -- Add default filetypes
+        vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+        -- Remove excluded filetypes
+        --- @param ft string
+        opts.filetypes = vim.tbl_filter(function(ft)
+          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+        end, opts.filetypes)
+
+        -- Additional settings for Phoenix projects
+        opts.settings = {
+          tailwindCSS = {
+            includeLanguages = {
+              elixir = "html-eex",
+              eelixir = "html-eex",
+              heex = "html-eex",
+            },
+          },
+        }
+      end,
+    },
+  },
+}
